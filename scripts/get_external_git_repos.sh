34151c7de1bfe0839ebe16a30aabee022ed22b1b
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2015-12-08 08:01:18 +0100

    add https://github.com/tjohann/led_dot_matrix_clock to external -> led_dot_matrix_clock is my clock project ... see $ARMEL_HOME/projects

--- scripts/get_external_git_repos.sh
+++ scripts/get_external_git_repos.sh
@@ -24,11 +24,13 @@
 #
 ################################################################################
 #
-# Date/Beginn :    20.11.2015/15.08.2015
+# Date/Beginn :    08.12.2015/15.08.2015
 #
-# Version     :    V0.08
+# Version     :    V0.09
 #
-# Milestones  :    V0.08 (nov 2015) -> rebase for arm926_sdk
+# Milestones  :    V0.09 (nov 2015) -> add led_dot_matrix_clock (see also
+#                                      $ARMEL_HOME/projects/led_dot_clock)
+#                  V0.08 (nov 2015) -> rebase for arm926_sdk
 #                                      some cleanups
 #                  V0.07 (sep 2015) -> add linux-lin driver repos
 #                  V0.06 (sep 2015) -> add our baalue repos
@@ -63,7 +65,7 @@
 #
 
 # VERSION-NUMBER
-VER='0.08'
+VER='0.09'
 
 # if env is sourced 
 MISSING_ENV='false'
@@ -82,6 +84,7 @@ MISSING_ENV='false'
 # libbaalue -> https://github.com/tjohann/libbaalue.git
 # baalued -> https://github.com/tjohann/baalued.git
 # sllin -> https://github.com/tjohann/sllin.git"
+# clock -> https://github.com/tjohann/led_dot_matrix_clock
 REPO='none'
 
 # PROTOCOL
@@ -122,6 +125,7 @@ my_usage()
     echo "| REPO: libbaalue -> our foundation lib                  |"
     echo "| REPO: baalued -> our baalue daemon                     |"
     echo "| REPO: sllin -> (my version of) linux lin driver        |"
+    echo "| REPO: clock -> my led-dot-matrix clock project         |"
     echo "|                                                        |"
     echo "| Valid network protocols:                               |"
     echo "| PROTOCOL: none or empty -> use the simple git          |"
@@ -239,7 +243,8 @@ set_repo_names()
     void_packages="://github.com/voidlinux/void-packages.git"
     libbaalue="://github.com/tjohann/libbaalue.git"
     baalued="://github.com/tjohann/baalued.git"
-    sllin="://github.com/tjohann/sllin.git"    
+    sllin="://github.com/tjohann/sllin.git"
+    clock="://github.com/tjohann/led_dot_matrix_clock"
     
     # array with all available repos
     repo_names_array[0]=${at91bootstrap}    
@@ -254,7 +259,8 @@ set_repo_names()
     repo_names_array[9]=${void_packages}
     repo_names_array[10]=${libbaalue}
     repo_names_array[11]=${baalued}
-    repo_names_array[12]=${sllin}    
+    repo_names_array[12]=${sllin}
+    repo_names_array[13]=${clock}    
 }
 
 
@@ -300,6 +306,9 @@ get_repo_name()
 	    ;;
 	'sllin')
 	    REPO_NAME="${PROTOCOL}${sllin}"
+	    ;;
+	'clock')
+	    REPO_NAME="${PROTOCOL}${clock}"
 	    ;;
 	*)
 	    echo "ERROR -> ${REPO} is no valid repo ... pls check"
